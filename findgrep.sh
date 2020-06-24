a9f1c07547c09e40c5a1bfa04c4a3cf0a411628e
n20012 <n20012@std.it-college.ac.jp>
2020-06-24 11:29:25 +0900

    add comment

--- findgrep.sh
+++ findgrep.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# $1 => search pattern, $2 => search directory
 pattern=$1
 derectory=$2
 if [ -z "$derectory" ]; then
+	# current directory
 	derectory=','
 fi
+
+# -n : print line number
+# -H : print the file name
 find "$derectory" -type f | xargs grep -nH "$pattern"
